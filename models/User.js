92154f297fe5af5c2aca77046c6c2725007b17ed
YonisHussein <yonismhussein@gmail.com>
2022-07-22 20:59:31 -0400

    User.js and index.js

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { truncate } = require('fs');
+const { Schema, model } = require('mongoose');
+const Thought = require('./Thought');
+
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/],
+    },
+    thoughts:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.lenght;
+});
+
+const User = model('User', userSchema);
+module.exports = User;
